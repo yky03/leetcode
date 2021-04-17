62f1283cf63673a5a31186459f794768ff307b80
yky03 <34213213+yky03@users.noreply.github.com>
2021-04-17 16:26:47 +0900

    Update 0001-two-sum.java

--- 0001-two-sum.java
+++ 0001-two-sum.java
@@ -1,6 +1,6 @@
 
-# https://leetcode.com/problems/two-sum/submissions/
-# 1. two-sum ( easy )
+// https://leetcode.com/problems/two-sum/submissions/
+// 1. two-sum ( easy )
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
@@ -29,4 +29,17 @@ class Solution {
         
         
     }
+}
+
+
+// other solution
+public int[] twoSum(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+        for (int j = i + 1; j < nums.length; j++) {
+            if (nums[j] == target - nums[i]) {
+                return new int[] { i, j };
+            }
+        }
+    }
+    throw new IllegalArgumentException("No two sum solution");
 }
